766e1bde81f131b118cc32e97c4729d33685a074
vaneck237 <vaneckbilongo@gmail.com>
2023-05-04 15:53:22 +0100

    change validityDate to validityDateFrom

--- src/actions.js
+++ src/actions.js
@@ -69,7 +69,7 @@ export function fetchHealthFacility(mm, healthFacilityUuid, healthFacilityCode) 
     "catchments{id, location{id, uuid, code, name}, catchment}",
     "validityFrom",
     "validityTo",
-    "program { edges{ node{id idProgram nameProgram validityDate}}}"
+    "program { edges{ node{id idProgram nameProgram validityDateFrom}}}"
   ];
   const payload = formatPageQuery("healthFacilities", filters, projections);
   return graphql(payload, "LOCATION_HEALTH_FACILITY");
